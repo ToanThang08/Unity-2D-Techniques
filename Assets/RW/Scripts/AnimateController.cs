57291e5c3164dd2ca87e67c730b86414218fa7c1
Toan Thang <box.dota@gmail.com>
2023-08-24 18:05:10 +0300

    Scripting the Animation

--- Assets/RW/Scripts/AnimateController.cs
+++ Assets/RW/Scripts/AnimateController.cs
@@ -4,4 +4,16 @@ public class AnimateController : MonoBehaviour
 {
     public Sprite[] spriteSet;
     public float fps;
+
+
+    public GameObject treeObject;
+    private float turnSpeed = 30f;
+
+    private void FixedUpdate()
+    {
+        if (treeObject != null)
+        {
+            treeObject.transform.Rotate(new Vector3(0f, 0f, turnSpeed * Time.fixedDeltaTime));
+        }
+    }
 }
